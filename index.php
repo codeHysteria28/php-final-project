b7861485ee439cbc8d5f5a2776ca6a6a81e90fc7
codeHysteria28 <graphicglobal67@gmail.com>
2025-04-14 11:06:29 +0100

    adding project structure with necesary files

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+// Created by:
+// Branislav Buna
+// TU Dublin
+// B00158771
+
+include "src/templates/header.php";
+
+?>
+
+
+
+<?php include "src/templates/footer.php" ?>
---
+++ src/templates/header.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="../../styles/style.css" rel="stylesheet"/>
+    <title>Welcome</title>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Learn2Code.io</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0 justify-content-end">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../courses.php">Courses</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../about.php">About Us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../contact.php">Contact</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../login.php">Sign In</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
